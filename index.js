a93cc95328cdaef587e8a5af6e7bd78096356571
Dmitry <dimon-babon@yandex.ru>
2023-02-13 16:52:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-function myFunc(arg) {
-  console.log(`arg was => ${arg}`);
+var http = require('http');
+
+async function fetchJSON (url, options) {
+  let f = await fetch(url, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...(options?.headers || {}),
+    },
+  });
+  let t = await f.text();
+  try {
+    return JSON.parse(t);
+  } catch (e) {
+    throw new e.constructor(`${e.message} in ${url}\n\n"${t}"`);
+  }
 }
 
-setTimeout(myFunc, 1500, 'funky');
+http.createServer(async (req, res) =>
+{
+    console.log("yo");
+    let r = await fetchJSON("https://api.ipify.org?format=json");
+    console.log("yoo");
+    console.log(r);
+    
+    res.write(r);
+    //res.write('Yo!');
+    res.end();
+}).listen(process.env.PORT || 3000);
